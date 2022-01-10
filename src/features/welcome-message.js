538ad21c3275fc539ac06187d5e24a80819be9f0
DSESabrina <97014117+DSESabrina@users.noreply.github.com>
2022-01-09 19:01:22 -0800

    Updated Welcome Message.

--- src/features/welcome-message.js
+++ src/features/welcome-message.js
@@ -10,7 +10,7 @@ module.exports = (client, instance) => {
             return
         }
 
-        channel.send(`Welcome ${member} to ${guild} We hope you enjoy your stay here, we recommend you head over to our Rules, and give them a read. We at Dragon Studios Entertainment want to make sure we can provide you the best place to keep updated, on our Game Projects, and updates on our community. The Development Team wish to bring you a a story driven game you will play over and over. Letting it expand your library of games, and mind to the Possiblities. Our Moto is Gaming is More than just a mere passion will you rise up to the Challenges our game's bring to you? See you out on the Fields Player One. `)
+        channel.send(`Welcome ${member} to ${guild} We hope you enjoy your stay here, we recommend you head over to the server rules, and give them a read. We at Dragon Studios Entertainment want to make sure we can provide you the best place to keep updated, on our Game Projects, and updates on our community. Wheather or not you are within the Dragon Studios Server, or in your own Community. `)
     })
 
 
